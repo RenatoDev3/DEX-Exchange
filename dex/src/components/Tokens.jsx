ab7b9b8131a7c05750f827ccf864c656fa1340a5
RScro <rscro@protonmail.com>
2023-10-09 13:23:45 +0200

    Added Tokens page & css

--- dex/src/components/Tokens.jsx
+++ dex/src/components/Tokens.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "../Tokens.css";
+import tokenList from "../tokenList.json";
 
 function Tokens() {
-  return <div>Tokens</div>;
+  return (
+    <div className="tokens">
+      <h2>Token List</h2>
+      <ul className="list-group">
+        <li className="list-group-info">
+          <p>#</p>
+          <p>Token Name</p>
+        </li>
+        {tokenList.map((token, index) => (
+          <li key={index}>
+            <div className="list-group-item">
+              <p>{index + 1}</p>
+              <img src={token.img} alt={token.ticker} className="token-logo" />
+              <h4>{token.name}</h4>
+              <p>{token.ticker}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Tokens;
